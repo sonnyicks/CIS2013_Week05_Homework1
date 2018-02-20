b307a404d1074c7b12df0f9afdd0e5e9fe3c8961
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-02-20 00:57:34 -0600

    Week 5 homework (final)

--- CIS2013_Week05_Homework1.cpp
+++ CIS2013_Week05_Homework1.cpp
@@ -19,50 +19,63 @@ cout << "Enter 2 digit month: ";
 cin >> month;
 cout << "Enter 4 digit year: ";
 cin >> year;
+cout << endl;
 
 start=startday(month, year);
 month_year (month, year);
-if ((month==1)||(month==3)||(month==5)||(month==7)||(month==8)||(month==10)||(month==12)){day31(int);}
-else if ((month==4)||(month==6)||(month==9)||(month==11)){day30(int);}
-else if ((month==2)&&(year%4==0)){day_feb_l(int);}
-else if (month==2){day_feb(int)}
-else {cout << "invalid entry.";}
+
+if ((month==1)||(month==3)||(month==5)||(month==7)||(month==8)||(month==10)||(month==12)){day31(start);}
+else if ((month==4)||(month==6)||(month==9)||(month==11)){day30(start);}
+else if ((month==2)&&(year%4==0)){day_feb_l(start);}
+else if (month==2){day_feb(start);}
+else {cout << "invalid entry for month.";}
 
 	
 	return 0;
 }
 
-void day31 (int){
+void day31 (int first){
+	if (first==1){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << " 1  2  3  4  5  6  7" << endl
 		 << " 8  9 10 11 12 13 14" << endl
 		 << "15 16 17 18 19 20 21" << endl
 		 << "22 23 24 25 26 27 28" << endl
 		 << "29 30 31" << endl;
+	}
+	if (first==2){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "    1  2  3  4  5  6" << endl
 		 << " 7  8  9 10 11 12 13" << endl
 		 << "14 15 16 17 18 19 20" << endl
 		 << "21 22 23 24 25 26 27" << endl
 		 << "28 29 30 31" << endl;
+	}
+	if (first==3){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "       1  2  3  4  5" << endl
 		 << " 6  7  8  9 10 11 12" << endl
 		 << "13 14 15 16 17 18 19" << endl
 		 << "20 21 22 23 24 25 26" << endl
 		 << "27 28 29 30 31" << endl;
+	}
+	if (first==4){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "          1  2  3  4" << endl
 		 << " 5  6  7  8  9 10 11" << endl
 		 << "12 13 14 15 16 17 18" << endl
 		 << "19 20 21 22 23 24 25" << endl
 		 << "26 27 28 29 30 31" << endl;
+	}
+	if (first==5){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "             1  2  3" << endl
 		 << " 4  5  6  7  8  9 10" << endl
 		 << "11 12 13 14 15 16 17" << endl
 		 << "18 19 20 21 22 23 24" << endl
 		 << "25 26 27 28 29 30 31" << endl;
+	}
+	if (first==6){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "                1  2" << endl
 		 << " 3  4  5  6  7  8  9" << endl
@@ -70,6 +83,8 @@ void day31 (int){
 		 << "17 18 19 20 21 22 23" << endl
 		 << "24 25 26 27 28 29 30" << endl
 		 << "31" << endl;
+	}
+	if (first==7){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "                   1" << endl
 		 << " 2  3  4  5  6  7  8" << endl
@@ -77,45 +92,59 @@ void day31 (int){
 		 << "16 17 18 19 20 21 22" << endl
 		 << "23 24 25 26 27 28 29" << endl
 		 << "30 31" << endl;
+	}
 }
 
-void day30 (int){
+void day30 (int first){
+	if (first==1){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << " 1  2  3  4  5  6  7" << endl
 		 << " 8  9 10 11 12 13 14" << endl
 		 << "15 16 17 18 19 20 21" << endl
 		 << "22 23 24 25 26 27 28" << endl
 		 << "29 30" << endl;
+	}
+	if (first==2){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "    1  2  3  4  5  6" << endl
 		 << " 7  8  9 10 11 12 13" << endl
 		 << "14 15 16 17 18 19 20" << endl
 		 << "21 22 23 24 25 26 27" << endl
 		 << "28 29 30" << endl;
+	}
+	if (first==3){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "       1  2  3  4  5" << endl
 		 << " 6  7  8  9 10 11 12" << endl
 		 << "13 14 15 16 17 18 19" << endl
 		 << "20 21 22 23 24 25 26" << endl
 		 << "27 28 29 30" << endl;
+	}
+	if (first==4){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "          1  2  3  4" << endl
 		 << " 5  6  7  8  9 10 11" << endl
 		 << "12 13 14 15 16 17 18" << endl
 		 << "19 20 21 22 23 24 25" << endl
 		 << "26 27 28 29 30" << endl;
+	}
+	if (first==5){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "             1  2  3" << endl
 		 << " 4  5  6  7  8  9 10" << endl
 		 << "11 12 13 14 15 16 17" << endl
 		 << "18 19 20 21 22 23 24" << endl
 		 << "25 26 27 28 29 30" << endl;
+	}
+	if (first==6){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "                1  2" << endl
 		 << " 3  4  5  6  7  8  9" << endl
 		 << "10 11 12 13 14 15 16" << endl
 		 << "17 18 19 20 21 22 23" << endl
 		 << "24 25 26 27 28 29 30" << endl;
+	}
+	if (first==7){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "                   1" << endl
 		 << " 2  3  4  5  6  7  8" << endl
@@ -123,94 +152,123 @@ void day30 (int){
 		 << "16 17 18 19 20 21 22" << endl
 		 << "23 24 25 26 27 28 29" << endl
 		 << "30" << endl;
+	}
 }
 
-void day_feb_l (int){
+void day_feb_l (int first){
+	if (first==1){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << " 1  2  3  4  5  6  7" << endl
 		 << " 8  9 10 11 12 13 14" << endl
 		 << "15 16 17 18 19 20 21" << endl
 		 << "22 23 24 25 26 27 28" << endl
 		 << "29" << endl;
+	}
+	if (first==2){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "    1  2  3  4  5  6" << endl
 		 << " 7  8  9 10 11 12 13" << endl
 		 << "14 15 16 17 18 19 20" << endl
 		 << "21 22 23 24 25 26 27" << endl
 		 << "28 29" << endl;
+	}
+	if (first==3){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "       1  2  3  4  5" << endl
 		 << " 6  7  8  9 10 11 12" << endl
 		 << "13 14 15 16 17 18 19" << endl
 		 << "20 21 22 23 24 25 26" << endl
 		 << "27 28 29" << endl;
+	}
+	if (first==4){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "          1  2  3  4" << endl
 		 << " 5  6  7  8  9 10 11" << endl
 		 << "12 13 14 15 16 17 18" << endl
 		 << "19 20 21 22 23 24 25" << endl
 		 << "26 27 28 29" << endl;
+	}
+	if (first==5){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "             1  2  3" << endl
 		 << " 4  5  6  7  8  9 10" << endl
 		 << "11 12 13 14 15 16 17" << endl
 		 << "18 19 20 21 22 23 24" << endl
 		 << "25 26 27 28 29" << endl;
+	}
+	if (first==6){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "                1  2" << endl
 		 << " 3  4  5  6  7  8  9" << endl
 		 << "10 11 12 13 14 15 16" << endl
 		 << "17 18 19 20 21 22 23" << endl
 		 << "24 25 26 27 28 29" << endl;
+	}
+	if (first==7){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "                   1" << endl
 		 << " 2  3  4  5  6  7  8" << endl
 		 << " 9 10 11 12 13 14 15" << endl
 		 << "16 17 18 19 20 21 22" << endl
 		 << "23 24 25 26 27 28 29" << endl;
+	}
 }
-void day_feb (int){
+void day_feb (int first){
+	if (first==1){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << " 1  2  3  4  5  6  7" << endl
 		 << " 8  9 10 11 12 13 14" << endl
 		 << "15 16 17 18 19 20 21" << endl
 		 << "22 23 24 25 26 27 28" << endl;
+	}
+	if (first==2){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "    1  2  3  4  5  6" << endl
 		 << " 7  8  9 10 11 12 13" << endl
 		 << "14 15 16 17 18 19 20" << endl
 		 << "21 22 23 24 25 26 27" << endl
 		 << "28" << endl;
+	}
+	if (first==3){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "       1  2  3  4  5" << endl
 		 << " 6  7  8  9 10 11 12" << endl
 		 << "13 14 15 16 17 18 19" << endl
 		 << "20 21 22 23 24 25 26" << endl
 		 << "27 28" << endl;
+	}
+	if (first==4){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "          1  2  3  4" << endl
 		 << " 5  6  7  8  9 10 11" << endl
 		 << "12 13 14 15 16 17 18" << endl
 		 << "19 20 21 22 23 24 25" << endl
 		 << "26 27 28" << endl;
+	}
+	if (first==5){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "             1  2  3" << endl
 		 << " 4  5  6  7  8  9 10" << endl
 		 << "11 12 13 14 15 16 17" << endl
 		 << "18 19 20 21 22 23 24" << endl
 		 << "25 26 27 28" << endl;
+	}
+	if (first==6){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "                1  2" << endl
 		 << " 3  4  5  6  7  8  9" << endl
 		 << "10 11 12 13 14 15 16" << endl
 		 << "17 18 19 20 21 22 23" << endl
 		 << "24 25 26 27 28" << endl;
+	}
+	if (first==7){
 	cout << "Su Mo Tu We Th Fr Sa" << endl
 		 << "                   1" << endl
 		 << " 2  3  4  5  6  7  8" << endl
 		 << " 9 10 11 12 13 14 15" << endl
 		 << "16 17 18 19 20 21 22" << endl
 		 << "23 24 25 26 27 28" << endl;
+	}
 }
 void month_year(int month, int year){
 	if (month==1){
@@ -257,7 +315,7 @@ int startday (int month, int year){
 	int century=(year/100);
 	int decade=(year%100);
 	if (year>=100){year=(year%100);}
-	
+	//This formula will calculate the first the month of a year falls. return 1-7 corresponds to Sun-Sat.
 	year=year/4;
 	year+=1;
 	if (month==1){year+=1;}
